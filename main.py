2ad8a95879afe20fb6b42a93be0a1ae306a67348
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-07-20 16:32:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from voyage_scraper import main
+import json
+from fastapi import FastAPI
+from dropbox import Dropbox
+import config
+
+
+def download_file(file):
+    token = config.key
+    dbx = Dropbox(token)
+    # read a file
+    with open(file, 'wb') as f:
+        metadata, result = dbx.files_download(path='/Voyage/voyages.json')
+        f.write(result.content)
+
+app = FastAPI()
+
+@app.get('/')
+async def scrape():
+    download_file('voyages.json')
+    # Opening JSON file
+    with open('voyages.json') as json_file:
+        data = json.load(json_file)
+    return data
